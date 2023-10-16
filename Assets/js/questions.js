9006a210ab36e8eecf45695966b631daba06e900
Nicholas Hudson <nick.hudson512@gmail.com>
2023-10-16 12:07:10 -0500

    final product

---
+++ Assets/js/questions.js
@@ -0,0 +1,40 @@
+// establish questions
+let questions = [
+    {
+      numb: 1,
+      question: 'Commonly used data types DO NOT include:',
+      options: ['strings', 'booleans', 'alerts', 'numbers'],
+      answer: 'alerts',
+    },
+    {
+      numb: 2,
+      question: 'The condition in an if / else statement is enclosed within ____.',
+      options: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+      answer: 'parentheses',
+    },
+    {
+      numb: 3,
+      question: 'Arrays in JavaScript can be used to store ____.',
+      options: [
+        'numbers and strings',
+        'other arrays',
+        'booleans',
+        'all of the above',
+      ],
+      answer: 'all of the above',
+    },
+    {
+      numb: 4,
+      question:
+        'String values must be enclosed within ____ when being assigned to variables.',
+      options: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+      answer: 'quotes',
+    },
+    {
+      numb: 5,
+      question:
+        'A very useful tool used during development and debugging for printing content to the debugger is:',
+      options: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+      answer: 'console.log',
+    },
+  ];
